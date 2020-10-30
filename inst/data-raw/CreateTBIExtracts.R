5c65e5095f0291a0aa29d8a6bed12ec263fdade2
nekrum <nekrum@gmail.com>
2020-10-29 22:04:02 -0600

    Agrega función de descarga y procesamiento

--- inst/data-raw/CreateTBIExtracts.R
+++ inst/data-raw/CreateTBIExtracts.R
@@ -11,14 +11,4 @@ rows.genes <- fread(cmd = "unzip -p inst/data-raw/tbi_data.zip rows-genes.csv")
 for(col in names(rows.genes))
   set(rows.genes, j = col, value = factor(rows.genes[[col]]))
 
-
-normalized.rna.measures <- fread(cmd = "unzip -p inst/data-raw/tbi_data.zip fpkm_table_normalized.csv")
-setnames(normalized.rna.measures, "gene_id \\ rnaseq_profile_id", "gene_id")
-normalized.rna.measures <- melt(
-  normalized.rna.measures,
-  id.vars = "gene_id",
-  variable.name = "rnaseq_profile_id"
-)
-normalized.rna.measures[, gene_id := as.factor(gene_id)]
-
-usethis::use_data(normalized.rna.measures, column.samples, rows.genes, overwrite = TRUE)
+usethis::use_data(column.samples, rows.genes, overwrite = TRUE)
